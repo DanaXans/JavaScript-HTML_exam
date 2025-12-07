75e5ac628daaa2688cc5d60fa440e7b6b8b62da4
DanaXans <danaxans@gmail.com>
2025-12-07 11:41:47 +0200

    change task-1.js, task-1.html, 0-480_task.css, desktop_task-1.css, tablet_task-1.css

--- Javascript_exam_task-1/task-1.js
+++ Javascript_exam_task-1/task-1.js
@@ -1,37 +1,84 @@
 let addButton = document.getElementById('add');
 let input_name = document.getElementById('input_name');
 let output = document.getElementById('output');
-
 let arr = [];
-let object = {};
-//Требуемые функции:
-// Пары «имя/значение» необходимо вводить в верхнее текстовое поле. Это текстовое поле
-// будет использоваться конечным пользователем для быстрого добавления пар «имя/значение» в список ниже.
-// Перед добавлением пары «Имя/Значение» в список необходимо проверить её синтаксис.
-// Если синтаксис неверен, добавлять пару «Имя/Значение» не следует.
-// Формат записи пары «Имя/Значение» показан ниже:
-// <имя> = <значение>
+///////////////////////////////////////////////////////////////
+
+let checkbox;
+let label;
+let paragraph;
 
 addButton.addEventListener('click', () => {
     const value = input_name.value;
-    value.match(/\s([9a-z-A-Z-а-я-А-Я\d]+)\s=\s([a-z-A-Z-а-я-А-Я\d]+)\s/)
-
     let textParts = value.split("=");
     let keyTxt = textParts[0]?.trim();
     let valueTxt = textParts[1]?.trim();
 
-    let ul = document.createElement('ul');
-    if (valueTxt === undefined || null) {
-        console.log('error')
+    checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('class', 'check-input');
+    label = document.createElement('label');
+    paragraph = document.createElement('p');
+
+    if (!value.match(/^\s*([a-z-A-Z-а-я-А-Я-0-9]+)\s*=\s*([a-z-A-Z-а-я-А-Я-0-9]+)\s*$/) || NaN || undefined) {
+        throw new Error('invalid input value');
     } else {
-        let li = document.createElement('li');
         const template = {name: `${keyTxt}`, value: `${valueTxt}`};
         arr.push(template);
-        ul.appendChild(li)
-        li.innerText += `${keyTxt}=${valueTxt} `;
-
+        paragraph.innerText += `${keyTxt}=${valueTxt} `;
+        output.append(label);
+        label.append(checkbox, paragraph);
     }
-    output.appendChild(ul);
     console.log(arr);
+
 })
 
+//При нажатии кнопки «Удалить» все выбранные элементы в списке будут удалены.
+
+let deleteButton = document.getElementById('delete');
+deleteButton.addEventListener('click', () => {
+    let checkbox = document.querySelector('input[type="checkbox"]');
+    if (checkbox.checked) {
+        output.label.delete()
+    }
+})
+
+//При нажатии кнопки «Сортировать по имени» список будет отсортирован по имени в порядке возрастания.
+let sortByName = document.getElementById('sort_by_name');
+sortByName.addEventListener('click', () => {
+    output.innerHTML = '';
+    const sorted = [...arr].sort((a, b) => a.name.length - b.name.length);
+    console.log(sorted);
+
+    for (const elem of sorted) {
+        checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('class', 'check-input');
+        label = document.createElement('label');
+        paragraph = document.createElement('p');
+
+        paragraph.innerText += `${elem.name}=${elem.value}`;
+        output.append(label);
+        label.append(checkbox, paragraph);
+    }
+})
+
+//При нажатии кнопки «Сортировать по значению» список будет отсортирован по значению в порядке возрастания.
+let sortByValue = document.getElementById('sort_by_value');
+sortByValue.addEventListener('click', () => {
+    output.innerHTML = '';
+    const sorted = [...arr].sort((a, b) => a.value.length - b.value.length);
+    console.log(sorted);
+
+    for (const elem of sorted) {
+        checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('class', 'check-input');
+        label = document.createElement('label');
+        paragraph = document.createElement('p');
+
+        paragraph.innerText += `${elem.name}=${elem.value}`;
+        output.appendChild(label);
+        label.append(checkbox, paragraph);
+    }
+})
