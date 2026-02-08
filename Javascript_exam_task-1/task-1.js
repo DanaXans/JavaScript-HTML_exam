4fae254b756c34813f24ad66b532bf7e003dc44c
DanaXans <danaxans@gmail.com>
2026-02-08 18:39:30 +0200

    change task-1.js, task-1.html

--- Javascript_exam_task-1/task-1.js
+++ Javascript_exam_task-1/task-1.js
@@ -1,84 +1,63 @@
 let addButton = document.getElementById('add');
-let input_name = document.getElementById('input_name');
+let input_name_value = document.getElementById('input_name_value');
+let deleteButton = document.getElementById('delete')
 let output = document.getElementById('output');
-let arr = [];
-///////////////////////////////////////////////////////////////
 
-let checkbox;
-let label;
-let paragraph;
+let arr = [];
+
+function render() {
+    output.innerHTML = '';
+
+    for (const elem of arr) {
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('id', 'checkboxes')
+        const label = document.createElement('label');
+        label.classList.add('element')
+
+        const paragraph = document.createElement('p');
+        paragraph.innerText = `${elem.name}=${elem.value}`;
+        output.append(label);
+        label.append(checkbox, paragraph);
+    }
+}
 
 addButton.addEventListener('click', () => {
-    const value = input_name.value;
+    const value = input_name_value.value;
     let textParts = value.split("=");
     let keyTxt = textParts[0]?.trim();
     let valueTxt = textParts[1]?.trim();
 
-    checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.setAttribute('class', 'check-input');
-    label = document.createElement('label');
-    paragraph = document.createElement('p');
-
     if (!value.match(/^\s*([a-z-A-Z-а-я-А-Я-0-9]+)\s*=\s*([a-z-A-Z-а-я-А-Я-0-9]+)\s*$/) || NaN || undefined) {
         throw new Error('invalid input value');
     } else {
-        const template = {name: `${keyTxt}`, value: `${valueTxt}`};
+        let template = {name: `${keyTxt}`, value: `${valueTxt}`};
         arr.push(template);
-        paragraph.innerText += `${keyTxt}=${valueTxt} `;
-        output.append(label);
-        label.append(checkbox, paragraph);
     }
-    console.log(arr);
-
-})
+    render();
+});
 
 //При нажатии кнопки «Удалить» все выбранные элементы в списке будут удалены.
-
-let deleteButton = document.getElementById('delete');
 deleteButton.addEventListener('click', () => {
-    let checkbox = document.querySelector('input[type="checkbox"]');
-    if (checkbox.checked) {
-        output.label.delete()
-    }
-})
+  const elements=document.querySelectorAll('.element');
+
+  arr=arr.filter((item,index)=>{
+      const checkbox=elements[index].querySelector('input');
+      return !checkbox.checked;
+  })
+    render();
+});
 
 //При нажатии кнопки «Сортировать по имени» список будет отсортирован по имени в порядке возрастания.
 let sortByName = document.getElementById('sort_by_name');
 sortByName.addEventListener('click', () => {
-    output.innerHTML = '';
-    const sorted = [...arr].sort((a, b) => a.name.length - b.name.length);
-    console.log(sorted);
-
-    for (const elem of sorted) {
-        checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        checkbox.setAttribute('class', 'check-input');
-        label = document.createElement('label');
-        paragraph = document.createElement('p');
-
-        paragraph.innerText += `${elem.name}=${elem.value}`;
-        output.append(label);
-        label.append(checkbox, paragraph);
-    }
-})
+    arr.sort((a, b) => a.name.length - b.name.length);
+    render();
+});
 
 //При нажатии кнопки «Сортировать по значению» список будет отсортирован по значению в порядке возрастания.
 let sortByValue = document.getElementById('sort_by_value');
 sortByValue.addEventListener('click', () => {
-    output.innerHTML = '';
-    const sorted = [...arr].sort((a, b) => a.value.length - b.value.length);
-    console.log(sorted);
-
-    for (const elem of sorted) {
-        checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        checkbox.setAttribute('class', 'check-input');
-        label = document.createElement('label');
-        paragraph = document.createElement('p');
-
-        paragraph.innerText += `${elem.name}=${elem.value}`;
-        output.appendChild(label);
-        label.append(checkbox, paragraph);
-    }
-})
+    arr.sort((a, b) => a.value.length - b.value.length);
+    render();
+});
