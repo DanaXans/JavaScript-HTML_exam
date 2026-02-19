b023bb34519b688a634dcd759ab6e98f65d29840
DanaXans <danaxans@gmail.com>
2026-02-19 20:39:54 +0200

    change post_stylesheet.css, post-details.html, script_posts.js

--- Javascript_exam_task-2/pages_wrapper/script_posts.js
+++ Javascript_exam_task-2/pages_wrapper/script_posts.js
@@ -8,23 +8,33 @@ fetch('https://jsonplaceholder.typicode.com/comments?postsId=1')
 
         let comments = json.filter(comment => comment.postId === posts_details.id);
         console.log(comments);
-
-        let wrapper_of_text = document.createElement('div');
+        ///
+        let object_post = document.createElement('div');
+        object_post.classList.add('object_post');
         let post_text = document.createElement('p');
-
+        //текст поста
         let heading_post = document.createElement('h3');
         heading_post.innerText = `${posts_details.id}. ${posts_details.title}`;
         post_text.innerText = `${posts_details.body}`;
 
-        document.body.append(wrapper_of_text);
-        wrapper_of_text.append(heading_post, post_text);
+
+        object_post.append(heading_post, post_text);
+        ///
+        let comments_of_post = document.createElement('div');
+        comments_of_post.classList.add('comments_of_post');
+        document.body.append(object_post, comments_of_post);
 
         for (const comment of comments) {
-            let comments_text = document.createElement('p');
-            let heading_comment = document.createElement('h4');
-            heading_comment.innerText = `${comment.name}`;
-            comments_text.innerText = `${comment.body}`;
-            wrapper_of_text.append(heading_comment, comments_text);
+            //текст комментов
+            let p_comment = document.createElement('p');
+            let h4_comment = document.createElement('h4');
+            h4_comment.innerText = `${comment.name}`;
+            p_comment.innerText = `${comment.body}`;
+
+            let p_and_h4_wrap = document.createElement('div');
+            p_and_h4_wrap.classList.add('p_and_h4_wrap');
+            p_and_h4_wrap.append(h4_comment, p_comment);
+            comments_of_post.append(p_and_h4_wrap);
         }
 
     })
