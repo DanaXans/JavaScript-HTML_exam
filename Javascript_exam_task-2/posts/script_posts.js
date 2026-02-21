b0f2c0ec44a70d610d4cec7c291bb615bdc9e7e9
DanaXans <danaxans@gmail.com>
2026-02-21 22:49:28 +0200

    finish second page

--- Javascript_exam_task-2/posts/script_posts.js
+++ Javascript_exam_task-2/posts/script_posts.js
@@ -6,20 +6,20 @@ fetch('https://jsonplaceholder.typicode.com/comments?postsId=1')
         const posts_details = JSON.parse(localStorage.getItem('details_of_posts'));
         console.log(posts_details);
 
-        let comments = json.filter(comment => comment.postId === posts_details.id);
+        const comments = json.filter(comment => comment.postId === posts_details.id);
         console.log(comments);
-        ///
+
         let object_post = document.createElement('div');
         object_post.classList.add('object_post');
         let post_text = document.createElement('p');
-        //текст поста
+
         let heading_post = document.createElement('h3');
         heading_post.innerText = `${posts_details.id}. ${posts_details.title}`;
         post_text.innerText = `${posts_details.body}`;
         let main_wrapper = document.createElement('div');
         main_wrapper.classList.add('main_wrapper');
         object_post.append(heading_post, post_text);
-        ///
+
         let comments_of_post = document.createElement('div');
         comments_of_post.classList.add('comments_of_post');
 
@@ -27,7 +27,6 @@ fetch('https://jsonplaceholder.typicode.com/comments?postsId=1')
         document.body.append(main_wrapper);
 
         for (const comment of comments) {
-            //текст комментов
             let p_comment = document.createElement('p');
             let h4_comment = document.createElement('h4');
             h4_comment.innerText = `${comment.name}`;
