8c1f2f41c36424033192da6a73d84f94187832c0
DanaXans <danaxans@gmail.com>
2026-02-21 22:46:37 +0200

    finish first page

--- Javascript_exam_task-2/main-page/script1.js
+++ Javascript_exam_task-2/main-page/script1.js
@@ -3,36 +3,33 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(json => {
         console.log(json);
 
-
         const arr = json;
 
         for (const user of arr) {
             let usersWrapper = document.createElement('div');
             usersWrapper.classList.add('userWrapper');
-            let generalWrapper= document.createElement('div');
+            let generalWrapper = document.createElement('div');
             generalWrapper.classList.add('generalWrapper');
-            let h3 = document.createElement('h3');
-            let p = document.createElement('p');
+
+            const heading_user_info = document.createElement('h3');
+            const paragraph_user_info = document.createElement('p');
+
             let buttonUrl = document.createElement('button');
             let textWrapper = document.createElement('div');
             textWrapper.classList.add('textWrapper');
-            p.innerText = `${user.id}.`;
-            h3.innerText = `${user.name}`;
 
-
+            paragraph_user_info.innerText = `${user.id}.`;
+            heading_user_info.innerText = `${user.name}`;
             buttonUrl.innerText = 'Details';
+
             buttonUrl.id = user.id;
 
             buttonUrl.addEventListener('click', () => {
                 localStorage.setItem('selectedUser', JSON.stringify(user));
                 window.location.href = "../users/user-details.html";
             });
-            document.body.append(textWrapper,generalWrapper);
-            textWrapper.append(p, h3);
-            generalWrapper.append(textWrapper,buttonUrl);
+            document.body.append(textWrapper, generalWrapper);
+            textWrapper.append(paragraph_user_info, heading_user_info);
+            generalWrapper.append(textWrapper, buttonUrl);
         }
-
-
     })
-
-
